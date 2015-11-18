7223796fa49ec7570c77f940cb6b61534658e440
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-18 17:04:24 +0100

    Project initialisation...

---
+++ Assets/Scripts/PacmanController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanController : MonoBehaviour {
+
+	public float speed = 0.1f;
+
+	private GameObject pacman;
+	private CharacterController pacmanCharacter;
+	private CardboardHead cardboardHead;
+
+	// Use this for initialization
+	void Start () {
+		pacman = GameObject.Find("Pacman");
+		pacmanCharacter = GetComponent<CharacterController> ();
+		cardboardHead = GetComponentInChildren<CardboardHead> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 headRotation = new Vector3(pacman.transform.eulerAngles.x, cardboardHead.transform.eulerAngles.y + 90, pacman.transform.eulerAngles.z);
+		pacman.transform.eulerAngles = headRotation;
+
+		Vector3 moveDirection = new Vector3 (cardboardHead.Gaze.direction.x, 0, cardboardHead.Gaze.direction.z);
+		pacmanCharacter.Move (moveDirection * speed);
+	}
+}
