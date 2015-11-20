dd77da2a72c14157d9180e1603bd51b216b06aa9
Vincent Kelleher <k2r79@hotmail.fr>
2015-11-20 16:30:42 +0100

    HUD start & Game phase prototypes

--- Assets/Scripts/PacmanController.cs
+++ Assets/Scripts/PacmanController.cs
@@ -5,18 +5,26 @@ public class PacmanController : MonoBehaviour {
 
 	public float speed = 0.1f;
 	public int lifes = 3;
+	public Vector3 startPosition = new Vector3 (-10.4f, 1.149f, -5.911f);
 
 	private GameObject pacman;
 	private CharacterController pacmanCharacter;
 	private CardboardHead cardboardHead;
+	private UnityEngine.UI.Text lifeHUD;
 
 	// Use this for initialization
 	void Start () {
 		pacman = GameObject.Find("Pacman");
 		pacmanCharacter = GetComponent<CharacterController> ();
 		cardboardHead = GetComponentInChildren<CardboardHead> ();
+		lifeHUD = GameObject.Find ("HUD Life").GetComponent<UnityEngine.UI.Text>();
+
+		lifeHUD.text = "Test";
+
+		SetStartPosition ();
+		UpdateHUD ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		Vector3 headRotation = new Vector3(pacman.transform.eulerAngles.x, cardboardHead.transform.eulerAngles.y + 90, pacman.transform.eulerAngles.z);
@@ -28,7 +36,29 @@ public class PacmanController : MonoBehaviour {
 
 	void OnControllerColliderHit(ControllerColliderHit hit) {
 		if (hit.collider.name == "Ghost") {
-
+			CollidedWithGhost();
 		}
+	}
+
+	void SetStartPosition () {
+		transform.position = startPosition;
+	}
+	
+	void CollidedWithGhost() {
+		SetStartPosition ();
+		
+		if (--lifes < 0) {
+			GameOver();
+		}
+
+		UpdateHUD ();
+	}
+
+	void UpdateHUD() {
+		lifeHUD.text = lifes.ToString();
+	}
+
+	void GameOver() {
+		Debug.Log ("Game Over !");
 	}
 }
