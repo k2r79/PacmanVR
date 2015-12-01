f69ec26f74b13fa0b80a5342b70a6bd6e59310ad
Vincent Kelleher <k2r79@hotmail.fr>
2015-12-01 10:29:02 +0100

    Intersections

---
+++ Assets/Scripts/BlinkyController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkyController : MonoBehaviour {
+	
+	public Vector3 startingPosition;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = startingPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
