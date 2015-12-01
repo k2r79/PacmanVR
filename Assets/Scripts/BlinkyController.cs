edf57b036a1d4c41856413c0b211fb937eda121c
Vincent Kelleher <k2r79@hotmail.fr>
2015-12-01 15:57:05 +0100

    Corrected PacMan movement

--- Assets/Scripts/BlinkyController.cs
+++ Assets/Scripts/BlinkyController.cs
@@ -1,17 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BlinkyController : MonoBehaviour {
+public class BlinkyController : GhostController {
 	
-	public Vector3 startingPosition;
-
-	// Use this for initialization
-	void Start () {
-		transform.position = startingPosition;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
---
+++ Assets/Scripts/GhostController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostController : MonoBehaviour {
+
+	public Vector3 startingPosition;
+	public Vector3 target;
+
+	protected NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start () {
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+
+		transform.position = startingPosition;
+		navMeshAgent.SetDestination (target);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
