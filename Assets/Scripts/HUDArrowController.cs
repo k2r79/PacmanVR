27e8898edb7593dfd95cd6614974a7b3bd548a76
Vincent Kelleher <k2r79@hotmail.fr>
2016-01-19 16:16:10 +0100

    HUD setup but faulty

---
+++ Assets/Scripts/HUDArrowController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDArrowController : MonoBehaviour {
+
+	public GameObject targetGhost;
+
+	private GameObject pacman;
+	
+	void Start () {
+		pacman = GameObject.Find ("Pacman");
+	}
+
+	void Update () {
+		if (Vector3.Distance (transform.position, pacman.transform.position) < 2.0f) {
+			this.enabled = true;
+
+			Vector3 pacmanVector = transform.position - pacman.transform.position;
+			Vector3 ghostVector = targetGhost.transform.position - pacman.transform.position;
+			Vector3 difference = pacmanVector - ghostVector;
+
+			float angleWithTarget = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+
+			Debug.Log(angleWithTarget);
+
+			Vector3 eulerAngles = new Vector3 (0, 0, angleWithTarget);
+			transform.localEulerAngles = eulerAngles;
+		} else {
+			this.enabled = false;
+		}
+	}
+}
